c807329b33a43fd09317355642cc3326bd57bb8b
= <balajivenkatask@gmail.com>
2023-06-02 10:48:21 +0530

    Installing Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         lg: `"nav nav""aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav" bg="">
         <Navbar></Navbar>
       </GridItem>
       <Show above="lg">
