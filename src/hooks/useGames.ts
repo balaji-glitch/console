f07c302053835d7a2aa2e252d2df7e89b6ace8cc
= <balajivenkatask@gmail.com>
2023-06-03 14:27:14 +0530

    Properties of Platform Score CropImage &  Skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../service/api-client";
+import { Controller } from "react-hook-form";
 export interface Platform {
   id: number;
   name: string;
@@ -23,12 +24,18 @@ const useGames = () => {
   const [game, setgame] = useState<Games[]>([]);
   const [error, seterror] = useState("");
   useEffect(() => {
+    const controller = new AbortController();
     setloading(true);
     apiClient
       .get<FetchGameResponse>("/games")
-      .then((res) => setgame(res.data.results))
+      .then((res) => {
+        setgame(res.data.results);
+        setloading(false);
+      })
       .catch((err) => console.log(seterror(err.message)));
-  });
+    60;
+    return () => controller.abort();
+  }, []);
 
   return { game, error, isloading };
 };
