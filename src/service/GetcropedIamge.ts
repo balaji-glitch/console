6f107735a5e9b7ca448f8a59c376e9524a3622f2
= <balajivenkatask@gmail.com>
2023-06-05 00:10:06 +0530

    No Image for Null

--- src/service/GetcropedIamge.ts
+++ src/service/GetcropedIamge.ts
@@ -1,5 +1,6 @@
+import Noimage from "../assets/no-image-placeholder-6f3882e0.webp";
 const GetcropedImageUrl = (Url: string) => {
-  if (!Url) return "";
+  if (!Url) return Noimage;
   const target = "media/";
   const index = Url.indexOf(target) + target.length;
   return Url.slice(0, index) + "crop/600/400/" + Url.slice(index);
