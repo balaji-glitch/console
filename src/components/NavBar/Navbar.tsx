d46c6a9e1faebd27f6f3fd5dd670aff28037af80
= <balajivenkatask@gmail.com>
2023-06-05 09:58:46 +0530

    Implementing SearchBar to Rterive Data

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,16 +1,17 @@
-import { HStack, IconButton, Image, Input, InputGroup } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../NavBar/logo.webp";
 import ColorModeSwitch from "../ColorModeSwitch";
-import { FaSearch } from "react-icons/fa";
+import Serach from "../SearchBox/Serach";
 
-const Navbar = () => {
+interface Props {
+  onSearch: (Search: string) => void;
+}
+
+const Navbar = ({ onSearch }: Props) => {
   return (
-    <HStack justifyContent={"space-between"} padding="8px">
+    <HStack padding="8px">
       <Image src={logo} boxSize="60px" />
-      <InputGroup width={"800px"}>
-        <Input type="tel" placeholder="Search" />
-        <IconButton aria-label="Search database" icon={<FaSearch />} />
-      </InputGroup>
+      <Serach onSearch={onSearch} />
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
